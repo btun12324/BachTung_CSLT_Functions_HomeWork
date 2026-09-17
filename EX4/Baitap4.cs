82f48ebca2d4079d0b7751dab0629ecb8e070a76
btun12324 <bachtung303207@gmail.com>
2026-09-17 11:31:21 +0700

    upload EX4 & EX5

---
+++ EX4/Baitap4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal class EX4
+{
+    static long Tinhgiaithua(int n)
+    {
+        long factorial = 1;
+
+        for ( int i = 1; i <= n; i++)
+        {
+            factorial = factorial * i;
+        }
+
+        return factorial;
+
+    }
+    public static void Main(string[] args)
+    {
+        Console.Write("Nhap vao so can tinh giai thua: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Giai thua cua {n} la {Tinhgiaithua(n)}");
+
+        Console.ReadKey();
+    }
+}
---
+++ EX5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+internal class EX5
+{
+    static string DaoNguocChuoi(string input)
+    {
+        char[] mangKyTu = input.ToCharArray();
+
+        Array.Reverse(mangKyTu);
+
+        return new string(mangKyTu);
+    }
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Write("Nhập chuỗi cần đảo ngược: ");
+        string chuoiGoc = Console.ReadLine();
+
+        string chuoiDaoNguoc = DaoNguocChuoi(chuoiGoc);
+
+        Console.WriteLine($"Chuỗi sau khi đảo ngược: {chuoiDaoNguoc}");
+
+        Console.ReadKey();
+    }
+}
